f53cbfaffc494f5921a3a0bfb6b2b8f33519334f
LuthMC <luthfi366822@gmail.com>
2025-03-01 02:14:28 +0700

    Update InstantVoidKill.phpp

--- src/Luthfi/InstantVoidKill/InstantVoidKill.php
+++ src/Luthfi/InstantVoidKill/InstantVoidKill.php
@@ -9,59 +9,141 @@ use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
 use pocketmine\utils\Config;
+use pocketmine\world\Position;
 
 class InstantVoidKill extends PluginBase implements Listener {
 
     /** @var Config */
     private $config;
 
-    /** @var int */
-    private $voidYLevel;
+    /** @var array */
+    private $toggledPlayers = [];
 
     public function onEnable(): void {
         $this->saveResource("config.yml");
-        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, ["void_y_level" => 0]);
-        $this->voidYLevel = $this->config->get("void_y_level", 0);
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+            "world_settings" => [
+                "world" => [
+                    "void_y_level" => 0
+                ]
+            ],
+            "teleport_instead_of_kill" => false,
+            "spawn_location" => [
+                "x" => 0,
+                "y" => 100,
+                "z" => 0,
+                "world" => "world"
+            ]
+        ]);
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onPlayerMove(PlayerMoveEvent $event): void {
         $player = $event->getPlayer();
-        if ($player->getPosition()->y < $this->voidYLevel) {
-            $player->kill();
+        $worldName = $player->getWorld()->getFolderName();
+        if (isset($this->toggledPlayers[$player->getName()])) {
+            return;
+        }
+
+        $voidYLevel = $this->config->get("world_settings")[$worldName]["void_y_level"] ?? 0;
+        if ($player->getPosition()->y < $voidYLevel) {
+            if ($this->config->get("teleport_instead_of_kill", false)) {
+                $spawnLocation = $this->config->get("spawn_location");
+                $world = $this->getServer()->getWorldManager()->getWorldByName($spawnLocation["world"]);
+                if ($world !== null) {
+                    $player->teleport(new Position(
+                        $spawnLocation["x"],
+                        $spawnLocation["y"],
+                        $spawnLocation["z"],
+                        $world
+                    ));
+                    $player->sendMessage("You were teleported to safety!");
+                }
+            } else {
+                $player->kill();
+            }
         }
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if ($command->getName() === "ivk") {
-            if (!$sender->hasPermission("ivk.use")) {
-                $sender->sendMessage("You do not have permission to use this command.");
-                return false;
-            }
+            switch ($args[0] ?? "") {
+                case "set":
+                    if (!$sender->hasPermission("ivk.use")) {
+                        $sender->sendMessage("You do not have permission to use this command.");
+                        return false;
+                    }
 
-            if (!isset($args[0])) {
-                $sender->sendMessage("Usage: /ivk set <y-level>");
-                return false;
-            }
+                    if (!isset($args[1]) || !isset($args[2])) {
+                        $sender->sendMessage("Usage: /ivk set <y-level> <world_name>");
+                        return false;
+                    }
 
-            if ($args[0] === "set") {
-                if (!isset($args[1])) {
-                    $sender->sendMessage("Usage: /ivk set <y-level>");
+                    $yLevel = (int)$args[1];
+                    $worldName = $args[2];
+
+                    if (!is_numeric($yLevel)) {
+                        $sender->sendMessage("Y level must be a number!");
+                        return false;
+                    }
+
+                    $worldSettings = $this->config->get("world_settings", []);
+                    $worldSettings[$worldName] = ["void_y_level" => $yLevel];
+                    $this->config->set("world_settings", $worldSettings);
+                    $this->config->save();
+
+                    $sender->sendMessage("Void Y level for '$worldName' set to $yLevel.");
+                    return true;
+
+                case "toggle":
+                    if (!$sender->hasPermission("ivk.toggle")) {
+                        $sender->sendMessage("You do not have permission to use this command.");
+                        return false;
+                    }
+
+                    if (!$sender instanceof Player) {
+                        $sender->sendMessage("This command can only be used in-game.");
+                        return false;
+                    }
+
+                    $playerName = $sender->getName();
+                    if (isset($this->toggledPlayers[$playerName])) {
+                        unset($this->toggledPlayers[$playerName]);
+                        $sender->sendMessage("InstantVoidKill Enabled!");
+                    } else {
+                        $this->toggledPlayers[$playerName] = true;
+                        $sender->sendMessage("InstantVoidKill Disabled!");
+                    }
+                    return true;
+
+                case "settp":
+                    if (!$sender->hasPermission("ivk.settp")) {
+                        $sender->sendMessage("You do not have permission to use this command.");
+                        return false;
+                    }
+
+                    if (!$sender instanceof Player) {
+                        $sender->sendMessage("This command can only be used in-game.");
+                        return false;
+                    }
+
+                    $location = $sender->getPosition();
+                    $this->config->set("spawn_location", [
+                        "x" => $location->getX(),
+                        "y" => $location->getY(),
+                        "z" => $location->getZ(),
+                        "world" => $location->getWorld()->getFolderName()
+                    ]);
+                    $this->config->set("teleport_instead_of_kill", true);
+                    $this->config->save();
+
+                    $sender->sendMessage("Spawn location set to your current position.");
+                    return true;
+
+                default:
+                    $sender->sendMessage("Usage: /ivk <set|toggle|settp>");
                     return false;
-                }
-
-                $yLevel = (int)$args[1];
-                if (!is_numeric($yLevel)) {
-                    $sender->sendMessage("Y level must be a number!");
-                    return false;
-                }
-
-                $this->voidYLevel = $yLevel;
-                $this->config->set("void_y_level", $yLevel);
-                $this->config->save();
-                $sender->sendMessage("Void Y level set to " . $yLevel);
-                return true;
             }
         }
         return false;
